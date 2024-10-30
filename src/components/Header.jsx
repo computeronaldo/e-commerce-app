6377b722ea720b67e2558e3225398681b1349bfb
Prajwal Choudhary <52006937+computeronaldo@users.noreply.github.com>
2024-10-30 12:53:55 +0530

    Fix: removed redundant useSelector statement

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,6 @@ import "./Header.css";
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const { user } = useSelector(state => state.user);
 
   const [searchQuery, setSearchQuery] = useState("");
 
